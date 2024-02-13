f9d533cc384b785752424f9b72c55820ea8889f8
Javier Colombera <jcolombera@alephee.com>
2024-02-13 11:32:20 -0300

    Operations to kafka topic

--- PermissionApp/Contracts/KafkaMessage.cs
+++ PermissionApp/Contracts/KafkaMessage.cs
@@ -3,4 +3,5 @@ namespace PermissionApp.Contracts;
 public class KafkaMessage
 {
     public Guid Id { get; set; }
+    public string NameOperation { get; set; }
 }
--- PermissionApp/Handlers/GetPermissionsQueryHandler.cs
+++ PermissionApp/Handlers/GetPermissionsQueryHandler.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
+using MassTransit;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using PermissionApp.Contracts;
 using PermissionApp.Domain;
 using PermissionApp.Infrastructure;
 
@@ -9,18 +11,27 @@ namespace PermissionApp.Handlers;
 public class GetPermissionsQueryHandler : IRequestHandler<GetPermissionsQuery, Employee?>
 {
     private readonly AppDbContext _dbContext;
-    private readonly IMapper _mapper;
+    private readonly ITopicProducer<KafkaMessage> _kafkaProducer;
 
-    public GetPermissionsQueryHandler(AppDbContext dbContext, IMapper mapper)
+    public GetPermissionsQueryHandler(AppDbContext dbContext, IMapper mapper,
+        ITopicProducer<KafkaMessage> kafkaProducer)
     {
         _dbContext = dbContext;
-        _mapper = mapper;
+        _kafkaProducer = kafkaProducer;
     }
-
     
-    public async Task<Employee?> Handle(GetPermissionsQuery request, CancellationToken cancellationToken) =>
-        await _dbContext.Employees.Include(x => x.Permissions)
+    public async Task<Employee?> Handle(GetPermissionsQuery request, CancellationToken cancellationToken)
+    {
+        await _kafkaProducer.Produce(
+            new KafkaMessage()
+            {
+                Id = Guid.NewGuid(),
+                NameOperation = "get"
+            }, cancellationToken);
+        
+        return await _dbContext.Employees.Include(x => x.Permissions)
             .ThenInclude(x => x.PermissionType)
             .Where(x => x.Id == request.EmployeeId)
-            .FirstOrDefaultAsync();
+            .FirstOrDefaultAsync(cancellationToken);
+    }
 }
